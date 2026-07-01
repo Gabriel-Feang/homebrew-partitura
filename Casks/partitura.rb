fad94ced347ca6aabebf6f236c8929b5c820e53e
GABRIEL FERREIRA ANGELO <163621651+Gabriel-Feang@users.noreply.github.com>
2026-07-01 22:59:13 +0000

    chore(cask): bump to 2.6.0

--- Casks/partitura.rb
+++ Casks/partitura.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "partitura" do
-  version "2.5.2"
+  version "2.6.0"
   sha256 :no_check
 
   # Releases live on GitHub Releases (private repo) and are streamed through the
